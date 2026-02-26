795ae5a0cf918cb661d7a7f1888b7fcb51669be5
Ankit Raj <ankitsingh154760@gmail.com>
2026-02-26 19:49:59 +0530

    home section is completed

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -3,7 +3,46 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src='media/largestBroker.svg' alt='largest Broker'></img>
+                </div>
+                <div className='col-6'></div>
+                  <h1>Largest stock broker in India</h1>
+                  <p>2+ million Zerodha clients contribute to over 15% of all retail order 
+                    volumes in India daily by trading and investing in: </p>
+                    <div className='row'>
+                        <div className='col-6'>
+                        <ul>
+                        <li>
+                            <p>Futures and Options</p>
+                        </li>
+                        <li>
+                            <p>Commodity derivatives</p>
+                        </li>
+                        <li>
+                            <p>Currency derivatives</p>
+                        </li>
+                    </ul>
+                </div>
+                        <div className='col-6'>
+                                <ul>
+                        <li>
+                            <p>Stocks &IPOs</p>
+                        </li>
+                        <li>
+                            <p>Direct mutual funds</p>
+                        </li>
+                        <li>
+                            <p>Bonds and Govt.security </p>
+                        </li>
+                    </ul>
+                        </div>
+                    </div>
+                    
+            </div>
+        </div>
      );
 }
 
