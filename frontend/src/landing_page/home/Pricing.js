16e02e922be37bd5a64ef17b3f618963a211e8a0
Ankit Raj <ankitsingh154760@gmail.com>
2026-03-01 09:32:20 +0530

    Pricing  component working

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -4,7 +4,20 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+       <div className='container'>
+        <div className='row'>
+            <div className='col-4'>
+                <h1 className='mb-3'>Unbeatable pricing</h1>
+                <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                 <a href='' className='mx-5 ' style={{textDecoration:"none"}}>See Pricing<i class="fa-solid fa-arrow-right"></i></a>
+                <div className='col-2'>
+                    <div className='col-6'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
      );
 }
 
