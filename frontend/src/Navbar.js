75857c2130d8eb4b55549a8ab75680dc57f1b7a6
Ankit Raj <ankitsingh154760@gmail.com>
2026-03-06 10:34:23 +0530

    complete Open account and Navbar section

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,74 +1,62 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
-    return (  
-        <div className='container'>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+  return (
+    <nav className="navbar navbar-expand-lg border-bottom bg-white">
+      <div className="container">
 
-                    <button 
-                        className="navbar-toggler" 
-                        type="button" 
-                        data-bs-toggle="collapse" 
-                        data-bs-target="#navbarSupportedContent" 
-                        aria-controls="navbarSupportedContent" 
-                        aria-expanded="false" 
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        {/* Logo */}
+        <a className="navbar-brand" href="/">
+          <img src="media/logo.svg" style={{ width: "120px" }} alt="Zerodha logo" />
+        </a>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
+        {/* Mobile Toggle */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
+        {/* Right Side Menu */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
 
-                            <li className="nav-item dropdown">
-                                <a 
-                                    className="nav-link dropdown-toggle" 
-                                    href="#" 
-                                    role="button" 
-                                    data-bs-toggle="dropdown" 
-                                    aria-expanded="false"
-                                >
-                                    Dropdown
-                                </a>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Signup</a>
+            </li>
 
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">About</a>
+            </li>
 
-                            <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Products</a>
+            </li>
 
-                        <form className="d-flex" role="search">
-                            <input 
-                                className="form-control me-2" 
-                                type="search" 
-                                placeholder="Search" 
-                                aria-label="Search"
-                            />
-                            <button className="btn btn-outline-success" type="submit">
-                                Search
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Pricing</a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Support</a>
+            </li>
+
+            {/* Menu icon */}
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                <i className="fa-solid fa-bars"></i>
+              </a>
+            </li>
+
+          </ul>
         </div>
-    );
+
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
